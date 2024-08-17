5d094281007497649d1f0bd3b0ce6be5e765c684
Sordi <rafael@consultornet.com.br>
2024-08-17 17:57:00 -0300

    including checking cookies exist

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,54 +2,101 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 
-import crypto from 'node:crypto'
+import crypto, { randomUUID } from 'node:crypto'
+import { checkSessionIdExist } from '../middlewares/check-sessionId-exist'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-    return { transactions }
-  })
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
 
-  app.get('/:id', async (request) => {
-    const getTransactionsParamsSchema = z.object({
-      id: z.string().uuid(),
-    })
+      return { transactions }
+    },
+  )
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async (request) => {
+      const getTransactionsParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
 
-    const { id } = getTransactionsParamsSchema.parse(request.params)
+      const { id } = getTransactionsParamsSchema.parse(request.params)
 
-    const transaction = await knex('transactions').where('id', id).first()
+      const { sessionId } = request.cookies
 
-    return {
-      transaction,
-    }
-  })
+      const transaction = await knex('transactions')
+        .where({ session_id: sessionId, id })
+        .first()
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      .sum('amount', { as: 'amount' })
-      .first()
+      return {
+        transaction,
+      }
+    },
+  )
 
-    return { summary }
-  })
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-  app.post('/', async (request, replay) => {
-    const createTransactionBodySchema = z.object({
-      title: z.string(),
-      amount: z.number(),
-      type: z.enum(['credit', 'debit']),
-    })
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
 
-    const { title, amount, type } = createTransactionBodySchema.parse(
-      request.body,
-    )
+      return { summary }
+    },
+  )
 
-    await knex('transactions').insert({
-      id: crypto.randomUUID(),
-      title,
-      amount: type === 'credit' ? amount : amount * -1,
-    })
+  app.post(
+    '/',
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async (request, replay) => {
+      const createTransactionBodySchema = z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(['credit', 'debit']),
+      })
 
-    return replay.status(201).send()
-  })
+      const { title, amount, type } = createTransactionBodySchema.parse(
+        request.body,
+      )
+
+      let sessionId = request.cookies.sessionId
+
+      if (!sessionId) {
+        sessionId = randomUUID()
+
+        replay.cookie('sessionId', sessionId, {
+          path: '/',
+          maxAge: 60 * 60 * 24 * 7, // 7 days
+        })
+      }
+
+      await knex('transactions').insert({
+        id: crypto.randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : amount * -1,
+        session_id: sessionId,
+      })
+
+      return replay.status(201).send()
+    },
+  )
 }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
+
+app.register(cookie)
+
+app.addHook('preHandler', async (request) => {
+  console.log(`[${request.method} ${request.url}]`)
+})
 
 app.register(transactionsRoutes, {
   prefix: 'transactions',
@@ -13,5 +21,5 @@ app
     port: env.PORT,
   })
   .then(() => {
-    console.log('Server running')
+    console.log('Server Started!')
   })
